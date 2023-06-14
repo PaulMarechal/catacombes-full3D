427320a19d697a8d68b8aff292a6858f02248632
Paul Marechal <paulmarechal785@hotmail.fr>
2023-06-14 12:00:28 +0200

    remove comment

--- src/displayRooms/script.js
+++ src/displayRooms/script.js
@@ -21,7 +21,6 @@ function addButtonDiv(elem, idElem, link, textButton, newDiv){
     if(idElem === "buttonAR"){
         elemCreated.setAttribute("title", "Augmented Reality for phones");
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
-            // elemCreated.style.backgroundColor = "#FFF"
             elemCreated.setAttribute("href", `https://catacombes.xyz/${NameRooms.roomInfos[roomNumber][0]}/AR`)
         } else {
             elemCreated.style.background = "#00000050"
