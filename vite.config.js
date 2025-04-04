9fa8a2591427f3f33b039f33500474ec795c9231
Paul Marechal <paulmarechal785@hotmail.fr>
2025-04-04 12:23:14 +0200

    Update Vite.conig for obfuscrator

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
-import { defineConfig } from 'vite'
-import path from 'path'
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
+import { defineConfig } from 'vite';
+import path from 'path';
+import obfuscator from 'rollup-plugin-obfuscator';
 
-export default {
+export default defineConfig({
     root: 'src/',
     publicDir: '../static/',
     base: './',
-    server:
-    {
+    server: {
         host: true,
         open: !isCodeSandbox 
     },
-    build:
-    {
+    build: {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true, 
-        chunkSizeWarningLimit: 1600
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            plugins: [
+                obfuscator({
+                    compact: true,
+                    controlFlowFlattening: true,
+                    rotateStringArray: true,
+                    stringArray: true,
+                    stringArrayEncoding: ['rc4'],
+                    stringArrayThreshold: 0.75,
+                    deadCodeInjection: true,
+                    selfDefending: true,
+                    disableConsoleOutput: true
+                })
+            ]
+        }
     }
-}
+});
