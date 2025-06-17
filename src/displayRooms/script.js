8bb957c9d6fcecf10a215832185a2843eba66d65
Paul Marechal <paulmarechal785@hotmail.fr>
2025-06-17 15:59:17 +0200

    Update script.js display all elem displayRoom page

--- src/displayRooms/script.js
+++ src/displayRooms/script.js
@@ -1,198 +1,160 @@
 import * as NameRooms from "../roomInfo.js";
 
-var roomNumber = 0;
+const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
 
-function addButtonDiv(elem, idElem, link, textButton, newDiv){
-    var elemCreated = document.createElement(elem); 
-    elemCreated.setAttribute("id", idElem);
-    elemCreated.classList.add(idElem); 
-    elemCreated.classList.add("buttonRedirect3D");
-    elemCreated.setAttribute("href", link);
-    // elemCreated.setAttribute("target", "_blank");
-    elemCreated.textContent = textButton;
-    newDiv.appendChild(elemCreated);
-
-    const elemToTest = document.getElementById(idElem);
-
-    function isWebXRSupported() {
-        return 'xr' in navigator && 'isSessionSupported' in navigator.xr;
-    }
-
-    if(idElem === "buttonAR"){
-        elemCreated.setAttribute("title", "Augmented Reality for phones");
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
-            elemCreated.setAttribute("href", `/AR/?room=${NameRooms.roomInfos[roomNumber][0]}`)
-        } else {
-            elemCreated.style.background = "#00000050"
-            elemCreated.setAttribute("href", "#");
-            elemCreated.style.cursor = "not-allowed"
-            elemCreated.style.opacity = "0.2"
-            elemCreated.classList.add("webXRDontWork")
-        }
-    } else if(idElem === "buttonVR"){
-        elemCreated.setAttribute("title", "Virtual Reality for VR headset");
-        if ('xr' in navigator) {
-            navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
-                if (supported) {
-                    elemCreated.setAttribute("href", `/AR/?room=${NameRooms.roomInfos[roomNumber][0]}`)
-                } else {
-                    elemCreated.setAttribute('href', '#');
-                    elemCreated.style.backgroundColor = "#00000050" 
-                    elemCreated.style.cursor = "not-allowed"
-                    elemCreated.style.opacity = "0.2"
-                    elemCreated.classList.add("webXRDontWork")
-                    elemCreated.addEventListener('click', (event) => {
-                        event.preventDefault();
-                    });
-                }
-            });
-        } else {
-            elemCreated.setAttribute('href', '#');
-            elemCreated.style.backgroundColor = "#00000050" 
-            elemCreated.style.cursor = "not-allowed!important"
-            elemCreated.style.opacity = "0.2"
-            elemCreated.classList.add("webXRDontWork")
-            elemCreated.addEventListener('click', (event) => {
-                event.preventDefault();
-            });
-        }
-    } 
+function disableLink(a) {
+    a.style.background = "#00000050";
+    a.style.cursor = "not-allowed";
+    a.style.opacity = "0.2";
+    a.classList.add("webXRDontWork");
+    a.addEventListener("click", e => e.preventDefault());
 }
 
-function addElement(roomNumber, language) {
-    var newDiv = document.createElement("div");
-    const language_site = document.querySelector("#language_site");
+function createButton(id, templateHref, text, title, roomId) {
+    const a = document.createElement("a");
+    a.id = id;
+    a.classList.add(id, "buttonRedirect3D");
+    if (title) a.setAttribute("title", title);
+    a.textContent = text;
 
-    newDiv.classList.add('parentDivRoomInfo');
+    const href = templateHref.replace("{room}", roomId);
+    if (id === "buttonAR") {
+        if (mobileRegex.test(navigator.userAgent)) {
+            a.href = href;
+        } else {
+            disableLink(a);
+        }
+    } else if (id === "buttonVR") {
+        if ('xr' in navigator && navigator.xr.isSessionSupported) {
+            navigator.xr.isSessionSupported('immersive-vr').then(supported => {
+                if (supported) a.href = href;
+                else disableLink(a);
+            });
+        } else disableLink(a);
+    } else {
+        // 3D always available
+        a.href = href;
+    }
+    return a;
+}
+
+function createRoomCard(room, langIndex) {
+    const [roomId, titleText, descEN, locationText, depthValue] = room;
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("parentDivRoomInfo_new");
 
     // Title
-    var title = document.createElement("h2");
-    title.classList.add('titleRoom');
-    title.textContent = `${NameRooms.roomInfos[roomNumber][1]}`
-    newDiv.appendChild(title);
-    
-    // Body
-    var img = document.createElement("img");
-    img.setAttribute("src", `https://catacombes.xyz/${NameRooms.roomInfos[roomNumber][0]}/${NameRooms.roomInfos[roomNumber][0]}.webp`);
-    img.setAttribute("alt", `${NameRooms.roomInfos[roomNumber][1]}`)
-    img.setAttribute("id", "imgRoom")
-    img.classList.add('imgRoom')
-    newDiv.appendChild(img);
-
-    // Room info
-    var text = document.createElement("p");
-    text.classList.add('textRoom');
-    text.textContent = `${NameRooms.roomInfos[roomNumber][language]}`
-
-    newDiv.appendChild(text);
+    const h2 = document.createElement("h2");
+    h2.classList.add("titleRoom_new");
+    h2.textContent = titleText;
+    newDiv.appendChild(h2);
 
     // Location
-    var location = document.createElement("p");
-    location.classList.add('adressRoom');
-    location.textContent = `Location : \n ${NameRooms.roomInfos[roomNumber][3]}\n\n Depth: ${NameRooms.roomInfos[roomNumber][4]}`
-    newDiv.appendChild(location);
+    const pLoc = document.createElement("p");
+    pLoc.classList.add("location_room");
+    pLoc.setAttribute("title", "Emplacement");
+    pLoc.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-map"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7l6 -3l6 3l6 -3v13l-6 3l-6 -3l-6 3v-13"/><path d="M9 4v13"/><path d="M15 7v13"/></svg>
+        ${locationText.replace(/\n/g, "<br/>")}
+    `;
+    newDiv.appendChild(pLoc);
 
-    addButtonDiv("a", "buttonVR", `https://catacombes.xyz/3D/?room=${NameRooms.roomInfos[roomNumber][0]}`, "VR", newDiv)
-    addButtonDiv("a", "button3D", `/3D/?room=${NameRooms.roomInfos[roomNumber][0]}`, "3D", newDiv)
-    addButtonDiv("a", "buttonAR", `https://catacombes.xyz/AR/?room=${NameRooms.roomInfos[roomNumber][0]}`, "AR", newDiv)
-    
+    // Depth
+    const pDepth = document.createElement("p");
+    pDepth.classList.add("deepth_room");
+    pDepth.setAttribute("title", "Profondeur approximative");
+    pDepth.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-viewport-tall"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 10v-7l3 3"/><path d="M9 6l3 -3"/><path d="M12 14v7l3 -3"/><path d="M9 18l3 3"/><path d="M18 3h1a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-1"/><path d="M6 3h-1a2 2 0 0 0 -2 2v14a2 2 0 0 0 2 2h1"/></svg>
+        ${depthValue}
+    `;
+    newDiv.appendChild(pDepth);
 
-    var currentDiv = document.querySelector('.displayRooms');
-    currentDiv.appendChild(newDiv);
+    // Info & Buttons
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("display_text_div_infos");
+
+    const pText = document.createElement("p");
+    pText.classList.add("textRoom_new");
+    pText.textContent = room[langIndex];
+    infoDiv.appendChild(pText);
+
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("display_buttons");
+    btnContainer.appendChild(createButton("buttonVR", "https://catacombes.xyz/3D/?room={room}", "VR", "Virtual Reality for VR headset", roomId));
+    btnContainer.appendChild(createButton("button3D", "/3D/?room={room}", "3D", null, roomId));
+    btnContainer.appendChild(createButton("buttonAR", "https://catacombes.xyz/AR/?room={room}", "AR", "Augmented Reality for phones", roomId));
+
+    infoDiv.appendChild(btnContainer);
+    newDiv.appendChild(infoDiv);
+    return newDiv;
 }
-function create_search_bar() {
-    var searchContainer = document.createElement("div");
-    searchContainer.classList.add("searchContainer");
 
-    var search_bar = document.createElement("input");
-    search_bar.setAttribute("type", "text");
-    search_bar.setAttribute("placeholder", "Rechercher une salle");
-    search_bar.classList.add("search_bar");
-    search_bar.addEventListener("input", filterRooms);
+function createSearchBar() {
+    const container = document.createElement("div");
+    container.classList.add("searchContainer");
 
-    var searchIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    searchIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    searchIcon.setAttribute("width", "24");
-    searchIcon.setAttribute("height", "24");
-    searchIcon.setAttribute("viewBox", "0 0 24 24");
-    searchIcon.setAttribute("fill", "none");
-    searchIcon.setAttribute("stroke", "currentColor");
-    searchIcon.setAttribute("stroke-width", "2");
-    searchIcon.setAttribute("stroke-linecap", "round");
-    searchIcon.setAttribute("stroke-linejoin", "round");
-    searchIcon.classList.add("icon", "icon-tabler", "icons-tabler-outline", "icon-tabler-search");
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Rechercher une salle";
+    input.classList.add("search_bar");
+    input.addEventListener("input", filterRooms);
 
-    var path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("stroke", "none");
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("width", "24");
+    svg.setAttribute("height", "24");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("fill", "none");
+    svg.setAttribute("stroke", "currentColor");
+    svg.setAttribute("stroke-width", "2");
+    svg.setAttribute("stroke-linecap", "round");
+    svg.setAttribute("stroke-linejoin", "round");
+    svg.classList.add("icon", "icon-tabler", "icon-tabler-search");
+
+    const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path1.setAttribute("d", "M0 0h24v24H0z");
     path1.setAttribute("fill", "none");
-
-    var path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path2.setAttribute("d", "M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0");
-
-    var path3 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const path3 = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path3.setAttribute("d", "M21 21l-6 -6");
 
-    searchIcon.appendChild(path1);
-    searchIcon.appendChild(path2);
-    searchIcon.appendChild(path3);
+    svg.append(path1, path2, path3);
+    container.append(input, svg);
 
-    searchContainer.appendChild(search_bar);
-    searchContainer.appendChild(searchIcon);
-
-    var displayRooms = document.querySelector(".displayRooms");
-    displayRooms.insertAdjacentElement("beforebegin", searchContainer);
+    const roomsContainer = document.querySelector(".displayRooms");
+    roomsContainer.insertAdjacentElement("beforebegin", container);
 }
 
-
 function filterRooms() {
-    var query = document.querySelector(".search_bar").value.toLowerCase();
-    var rooms = document.querySelectorAll(".parentDivRoomInfo");
-
-    rooms.forEach(room => {
-        var title = room.querySelector(".titleRoom").textContent.toLowerCase();
+    const query = document.querySelector(".search_bar").value.toLowerCase();
+    document.querySelectorAll(".parentDivRoomInfo_new").forEach(room => {
+        const title = room.querySelector(".titleRoom_new").textContent.toLowerCase();
         if (title.includes(query)) {
-            room.style.display = "inline-grid"; 
-            requestAnimationFrame(() => { 
-                room.style.opacity = "1"; 
-            });
+            room.style.display = "inline-grid";
+            requestAnimationFrame(() => room.style.opacity = "1");
         } else {
-            room.style.opacity = "0"; 
-            setTimeout(() => {
-                room.style.display = "none"; 
-            }, 500);
+            room.style.opacity = "0";
+            setTimeout(() => room.style.display = "none", 500);
         }
     });
 }
 
 function initializeRooms() {
-    for (let i = 0; i < NameRooms.roomInfos.length; i++) {
-        addElement(i, 2);
-        roomNumber++;
-    }
-
-    const language_site = document.querySelector("#language_site");
-    language_site.addEventListener("click", function() {
-        const textRoom = document.querySelectorAll(".textRoom");
-        for(let i = 0; i < textRoom.length; i++) {
-            if(language_site.innerText === "FR"){
-                textRoom[i].textContent = `${NameRooms.roomInfos[i][9]}`;
-            } else {
-                textRoom[i].textContent = `${NameRooms.roomInfos[i][2]}`;
-            }
-        }
-        language_site.innerText = (language_site.innerText === "FR") ? "EN" : "FR";
+    const container = document.querySelector(".displayRooms");
+    NameRooms.roomInfos.forEach((room, idx) => {
+        container.appendChild(createRoomCard(room, 2));
     });
-    
-    create_search_bar();
+
+    const langBtn = document.querySelector("#language_site");
+    langBtn.addEventListener("click", () => {
+        document.querySelectorAll(".textRoom_new").forEach((p, idx) => {
+            p.textContent = langBtn.innerText === "FR" ? NameRooms.roomInfos[idx][9] : NameRooms.roomInfos[idx][2];
+        });
+        langBtn.innerText = langBtn.innerText === "FR" ? "EN" : "FR";
+    });
+
+    createSearchBar();
 }
 
-initializeRooms();
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    window.addEventListener("resize", function() {
-        window.location.reload();
-    });
-}
-
-console.log("Developed by 🍔 by Paul Maréchal");
+document.addEventListener("DOMContentLoaded", initializeRooms);
