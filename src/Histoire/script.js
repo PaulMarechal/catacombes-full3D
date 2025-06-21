81673de435d19deb0670f4bde80cd1a5dc63fda1
Paul Marechal <paulmarechal785@hotmail.fr>
2025-06-21 11:00:33 +0200

    Continue to add infos to Histoire page

--- src/Histoire/script.js
+++ src/Histoire/script.js
@@ -1,1 +1,30 @@
 console.log("Developed by 🍔 by Paul Maréchal")
+
+function display_text_histoire(title_div, text_div){
+    const title = document.getElementById(title_div)
+    const text = document.getElementById(text_div)
+
+    const main_text_infos_div = document.querySelectorAll("#main_text_infos_div div")
+
+    title.addEventListener("mouseover", ()=>{
+
+        main_text_infos_div.forEach((main_text) => {
+            main_text.style.opacity = "0"
+            setTimeout(() => {
+                main_text.style.display = "none"
+            }, 490);
+        })
+
+        setTimeout(() => {
+            text.style.display = "block"
+        }, 500);
+        
+        setTimeout(() => {
+            text.style.opacity = "1"
+        }, 510);
+    })
+}
+
+display_text_histoire("formation_geologique_title", "formation_geologique_div")
+display_text_histoire("exploitation_carriere_title", "exploitation_carrieres")
+display_text_histoire("techniques_creusement_title", "techniques_creusement")
